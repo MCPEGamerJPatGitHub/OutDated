107e8ada8ccb0fb86a9f4b8f975486bb7e7db767
Angelo Vidrio <omgk45@gmail.com>
2015-04-25 23:49:48 -0700

    Update OutDated.php

--- src/OutDated/OutDated.php
+++ src/OutDated/OutDated.php
@@ -32,12 +32,6 @@ public function onDPRPacket(DataPacketReceiveEvent $ev){
                         $ev->setCancelled(true);
                     }
                 break;
-                                
-                public function onDisable() {
-		$this->getLogger()->info(TextFormat::RED . "OutDated 1.0 Disabled!");
-	                        }
-                        }
-                }
              
                 default:
              
