7578077f38f834cf2bfbdd0c583e3429f563365d
DuncanA <mccleanduncan03@hotmail.com>
2015-04-26 07:08:05 +0100

    Added more stuff

--- scr/OutDated/OutDated.php
+++ scr/OutDated/OutDated.php
@@ -2,7 +2,17 @@
 
 namespace '/OutDated'
 
-use /pocketmine/plugin/PluginBase;
+use pocketmine/plugin/PluginBase;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+class OutDated extends PluginBase implements Listener {
+
+public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getLogger()->info(TextFormat::GREEN . "OutDated 1.0 Enabled!");
+                }
 
 public function onDPRPacket(DataPacketReceiveEvent $ev){
         $p = $ev->getPlayer();
